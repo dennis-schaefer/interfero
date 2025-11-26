a7168859b7fd1de92b25e65d39270f4162b25e58
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-11-26 23:30:58 +0100

    Created Spring Boot Project

---
+++ src/main/java/io/interfero/InterferoApplication.java
@@ -0,0 +1,13 @@
+package io.interfero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterferoApplication
+{
+    static void main(String[] args)
+    {
+        SpringApplication.run(InterferoApplication.class, args);
+    }
+}
