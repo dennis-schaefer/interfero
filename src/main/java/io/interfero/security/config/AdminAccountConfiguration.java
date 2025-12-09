5fd8540c0416196a9b9d1612e8cd447416699907
dennis-schaefer <dennis.schaefer94@gmail.com>
2025-12-09 16:56:31 +0100

    Added @Nullable annotations

--- src/main/java/io/interfero/security/config/AdminAccountConfiguration.java
+++ src/main/java/io/interfero/security/config/AdminAccountConfiguration.java
@@ -2,6 +2,7 @@ package io.interfero.security.config;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import org.jspecify.annotations.Nullable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,7 +14,7 @@ public class AdminAccountConfiguration
     private final String username;
     private final String password;
 
-    AdminAccountConfiguration(String username, String password)
+    AdminAccountConfiguration(@Nullable String username, @Nullable String password)
     {
         if (password == null || password.isBlank())
             throw new IllegalArgumentException("Admin account password must be provided and cannot be blank.");
