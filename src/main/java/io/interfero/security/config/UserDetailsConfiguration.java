3c1331f4fb60fbbf770b883dbbafbc78e5a5c62b
dennis-schaefer <dennis.schaefer94@gmail.com>
2025-12-09 17:08:08 +0100

    Added property import for admin account

--- src/main/java/io/interfero/security/config/UserDetailsConfiguration.java
+++ src/main/java/io/interfero/security/config/UserDetailsConfiguration.java
@@ -1,5 +1,7 @@
 package io.interfero.security.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,7 +9,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Slf4j
 @Configuration
+@EnableConfigurationProperties(AdminAccountConfiguration.class)
 public class UserDetailsConfiguration
 {
     @Bean
@@ -20,6 +24,8 @@ public class UserDetailsConfiguration
     public UserDetailsService userDetailsService(AdminAccountConfiguration adminAccountConfiguration)
     {
         var user = adminAccountConfiguration.toUserDetails();
+        log.info("Configured admin account: '{}'", user);
+
         return new InMemoryUserDetailsManager(user);
     }
 }
