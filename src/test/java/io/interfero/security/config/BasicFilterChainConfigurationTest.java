76be52ef8445d7175bf896148be1b86593aca7ff
dennis-schaefer <dennis.schaefer94@gmail.com>
2025-12-22 20:19:01 +0100

    Enabled Spring Docs in integration tests

--- src/test/java/io/interfero/security/config/BasicFilterChainConfigurationTest.java
+++ src/test/java/io/interfero/security/config/BasicFilterChainConfigurationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Profile("it")
 class BasicFilterChainConfigurationTest
 {
     @Autowired
