a9354ba8932836e1599420d727a76579dbf5369b
dennis-schaefer <dennis.schaefer94@gmail.com>
2025-12-25 22:40:44 +0100

    Refactored protected page component to only show spinner with 150ms delay

--- src/main/frontend/src/features/security/ProtectedPage.tsx
+++ src/main/frontend/src/features/security/ProtectedPage.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from './AuthContext';
 import {Spinner} from "@/components/ui/spinner.tsx";
 
@@ -8,8 +9,29 @@ interface ProtectedPageProps {
 
 export function ProtectedPage({ children }: ProtectedPageProps) {
     const { isAuthenticated, loading, login } = useAuth();
+    const [showSpinner, setShowSpinner] = useState(false);
 
-    if (loading) {
+    useEffect(() => {
+        if (!loading)
+        {
+            setShowSpinner(false);
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            setShowSpinner(true);
+        }, 150);
+
+        return () => {
+            clearTimeout(timer);
+            setShowSpinner(false);
+        };
+    }, [loading]);
+
+    if (isAuthenticated)
+        return <>{children}</>;
+
+    if (showSpinner && loading) {
         return (
             <div className={"w-full h-full absolute flex justify-center items-center"}>
                 <Spinner className={"size-10"} />
@@ -17,10 +39,8 @@ export function ProtectedPage({ children }: ProtectedPageProps) {
         );
     }
 
-    if (!isAuthenticated) {
+    if (!isAuthenticated && !loading) {
         login();
         return null;
     }
-
-    return <>{children}</>;
 }
